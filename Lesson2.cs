3b2828c313375d01fedb219b6f327b53916ca412
hooi yong hua <devyonghua@gmail.com>
2018-06-18 11:10:43 +0800

    Create Lesson2.cs
    // Q. Write Basic C# Program that Reads a variable and dosent close 
    
    //Output : 55

---
+++ Lesson2.cs
@@ -0,0 +1,28 @@
+// Q. Write Basic C# Program that Reads a variable and dosent close 
+
+
+
+
+
+
+
+using System;
+
+namespace ConsoleAppbasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int variable = 55;
+
+            Console.WriteLine(variable);// Note :  COde Closes Immediately 
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
+//Output : 55 
