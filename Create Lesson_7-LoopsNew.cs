b8f6536a8b058cd26ab347e647cd424f5756bcb3
hooi yong hua <devyonghua@gmail.com>
2018-06-18 16:25:12 +0800

    Create  Create Lesson_7-LoopsNew.cs
    Copied Over From 1. 
    Reason : its no on 1 on sololearn

---
+++ Create Lesson_7-LoopsNew.cs
@@ -0,0 +1,18 @@
+
+//Easier way to increment is to put var++ in loop condition , Example code 
+
+using System;
+
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            while(++num < 6) 
+                Console.WriteLine(num);
+        }
+    }
+}
